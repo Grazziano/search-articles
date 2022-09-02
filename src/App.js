93fa7fcfc0537c7e91e46a55774d59b3cc60598a
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-02 20:29:30 -0300

    Configuring browser-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
