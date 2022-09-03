45a55b27f00e21b77aff0a3bdddaa0a1a76ad22a
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-09-02 21:00:32 -0300

    Showing article links

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,23 +20,31 @@ function Home() {
 
   return (
     <div>
-      <h1>Home Page</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={({ target }) => setQuery(target.value)} />
         <button type="submit">Search</button>
       </form>
-      {articles.length > 0 &&
-        articles.map((article) => {
-          return (
-            <div key={article._source.id}>
-              <p>{article._source.authors}</p>
-              <p>{article._source.type}</p>
-              <p>{article._source.title}</p>
-              <p>{article._source.description}</p>
-              <hr />
-            </div>
-          );
-        })}
+      <section>
+        {articles.length > 0 &&
+          articles.map((article) => {
+            return (
+              <div key={article._source.id}>
+                <h2>{article._source.title}</h2>
+                <p>{article._source.authors}</p>
+                <p>{article._source.type}</p>
+                <p>{article._source.description}</p>
+                {article._source.urls.map((link, index) => (
+                  <p key={index}>
+                    <a href={link} target="_blank" rel="noreferrer">
+                      {link}
+                    </a>
+                  </p>
+                ))}
+                <hr />
+              </div>
+            );
+          })}
+      </section>
     </div>
   );
 }
